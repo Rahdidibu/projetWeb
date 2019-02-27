89678da2c3141916be2b9e1566e6da49893a5beb
mbernard <matt35231@gmail.com>
2019-02-27 15:46:05 +0100

    Add GMaps

--- src/Controller/AdvertController.php
+++ src/Controller/AdvertController.php
@@ -8,7 +8,7 @@ use Twig\Environment;
 class AdvertController
 {
     public function index(Environment $twig){
-        $content = $twig->render("index.html.twig");
+        $content = $twig->render("maps.html.twig");
         //$content = "Hello World";
         return new Response($content);
     }
