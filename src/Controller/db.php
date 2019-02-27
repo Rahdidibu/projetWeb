a6ddbe77bcbc51c639b921ac5286a84c546c4e23
shieldstorming <nicolas.lucas.50@gmail.com>
2019-02-27 09:29:11 +0100

    MAJ BDD

--- src/Controller/db.php
+++ src/Controller/db.php
@@ -4,33 +4,36 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
+use \PDO;
+
 
 class db
 {
   const ID = "root";
   const PASSWORD = "";
   const DBNAME = "projetWeb";
+  private $donnee;
+
   public function connection(){
     try
     {
       $bdd = new PDO('mysql:host=localhost;dbname=projetWeb;charset=utf8', 'root', '');
-      //$bdd = new PDO('mysql:host=localhost;dbname='DBNAME';charset=utf8', 'ID', 'PASSWORD');
+      //  $bdd = new PDO('mysql:host=localhost;dbname="DBNAME";charset=utf8', ID, PASSWORD);
+      $this->getAllFormation($bdd);
+      $bdd=null;
     }
     catch (Exception $e)
     {
       die('Erreur : ' . $e->getMessage());
     }
-    
+return new Response($this->donnee[0]); //Affiche première élément tab --> pour ne pas avoir d'erreur
   }
 
-  public function getAllFormation(){
+  public function getAllFormation($bdd){
     $result = $bdd->query('SELECT etablissement_lib, comp_lib, zone_lib, mention_lib, etablissement_zone_lon ,etablissement_zone_lat from formation
       inner join etablissement on formation.etablissement_id = etablissement.etablissement_id
       inner join zone on formation.zone_id = zone.zone_id
       inner join composante on formation.comp_id = composante.comp_id');
-    }
-
-    public function deconnection(){
-
+      $this->donnee=$result->fetch();
     }
   }
